9db4aae32794d75a22577b2af4a56f8b58efd2df
Yuri Lee <yurilee.tech@gmail.com>
2022-05-18 21:40:31 +0900

    📰 Feat: add Header right elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,5 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
@@ -19,7 +21,18 @@ function Header() {
           <li className="headerLink">My List</li>
         </ul>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   )
 }
